a61b13add53819363c1779738852cff0c67aebd9
Rajbir Malik <cs1170416@iitd.ac.in>
2019-09-12 15:23:03 +0530

    Minor changes in blurring.py

--- 2/blurring.py
+++ 2/blurring.py
@@ -3,16 +3,18 @@ import sys
 import numpy as np
 import cv2 as cv
 from mio import read, write, show, trace
+from noise import add_noise
 
 
 # Function for getting disc-shaped h(x, y)
 # @param n : radius of kernel/disc
-def disc(n: int):
+# @param r : ratio of kernel/disc radius (default 1)
+def disc(n: int, r: int = 1):
     try:
         _m = np.ones((2*n+1, 2*n+1))
         for i in range(n+1):
             for j in range(n+1):
-                if ((i-n)**2 + (j-n)**2)**0.5 > n:
+                if ((i-n)**2 + (j-n)**2)**0.5 > n/r:
                     _m[i, j] = 0
                     _m[2*n-i, j] = 0
                     _m[2*n-i, 2*n-j] = 0
@@ -27,16 +29,19 @@ def disc(n: int):
 # @param _n : size of the kernel
 def disc_blur(_img: np.array, _n: int):
     try:
-        _nimg = cv.filter2D(_img, cv.CV_8U, disc(_n))
+        _nimg = cv.filter2D(_img, cv.CV_8U, disc(_n, 3))
         if _nimg is None:
             raise ValueError('Convolution unsucessful.')
         return _nimg
     except:
         trace()
 
+
 # Main
 if __name__ == "__main__":
     try:
-        show(disc_blur(read(sys.argv[1]), int(sys.argv[2])))
+        _nimg = disc_blur(read(sys.argv[1]), int(sys.argv[2]))
+        _nimg = add_noise(_nimg, 'G', 20)
+        write(sys.argv[3], _nimg)
     except:
         trace()
