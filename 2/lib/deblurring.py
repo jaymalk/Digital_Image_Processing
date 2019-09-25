e7665c71e7dba8f09c0eded8c4a30b00cee86094
Rajbir Malik <cs1170416@iitd.ac.in>
2019-09-25 21:18:36 +0530

    Added edge preserving deblurring

--- 2/lib/deblurring.py
+++ 2/lib/deblurring.py
@@ -7,7 +7,9 @@ import numpy as np
 import cv2 as cv
 
 from fft import FFT, IFFT
-from mio import trace
+from mio import show, trace
+
+from denoising import total_variation_norm
 
 # Weiner Filtering...
 # param _img : noisy/blurry image
@@ -42,6 +44,53 @@ def weiner_(_img: np.ndarray, _h: np.ndarray, SNR: float = 0.00000001, _BW: bool
         K = SNR
         # Getting the inverse filter
         H_w = (np.conj(H)) * (1/(abs(H)**2 + K))
+        # Getting the image back...
+        F_rec = G * H_w
+        # Coming back to spatial domain
+        f_rec = np.real(IFFT(F_rec))
+        return np.fft.fftshift(f_rec)
+    except:
+        trace()
+
+
+
+# Trying for edge preserving deblurring
+def edge_preserving_deblurring(_img: np.ndarray, _h: np.ndarray, _lam: float = 1, _BW: bool = True, __show: bool = False):
+    try:
+        # Getting the optimal DFT size
+        __shp = _img.shape[:2]
+        '''
+        Getting fourier transforms (all needed)
+        Sensing
+        g = h*f + n (Real domain)
+        G = HF + N (Fourier Domain)
+        & weiner filter...
+        H* / (abs(H)**2 + K),
+        where K can be either of |N(u,v)|/|F(u,v)| or |N|/|F| (over complete matrix)
+        & recovered image  GH*
+        '''
+
+        # If the image is colored (BGR)
+        if not _BW:
+            __temp = np.zeros(_img.shape)
+            for _i in [0,1,2]:
+                __temp[:,:,_i] = edge_preserving_deblurring(_img[:,:,_i], _h, _lam=_lam, _BW=True)
+            return __temp
+        # else
+        H = FFT(_h, __shp)
+        G = FFT(_img, __shp)
+        # Getting TV norm
+        _norm, _grad = total_variation_norm(_img)
+        #SHOW
+        if __show:
+            show(_grad)
+        # Getting TV FFT
+        _L = FFT(_grad, _img.shape)
+        #SHOW
+        if __show:
+            show(abs(_L)/abs(_L).max())
+        # Getting the inverse filter
+        H_w = (np.conj(H)) * (1/(abs(H)**2 + _lam/abs(_L)))
         # Getting the image back...
         F_rec = G * H_w
         # Coming back to spatial domain
