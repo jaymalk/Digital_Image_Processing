a07db9ac7948976eb830efe7d520893269bbfa35
Rajbir Malik <cs1170416@iitd.ac.in>
2019-09-25 22:34:46 +0530

    Added all needed files

--- 2/lib/deblurring.py
+++ 2/lib/deblurring.py
@@ -11,11 +11,10 @@ from mio import show, trace
 
 from denoising import total_variation_norm
 
-# Weiner Filtering...
+# Weiner Filtering... I
 # param _img : noisy/blurry image
 # param _fest : estimate of original image
-# param _h : estimate of deblurring function (PSF)
-# param _n : estimate of noise distribution
+# param SNR: snr value estimate for the image
 # param _BW : whether the image is black and white
 def weiner_(_img: np.ndarray, _h: np.ndarray, SNR: float = 0.00000001, _BW: bool = True):
     try:
@@ -44,6 +43,49 @@ def weiner_(_img: np.ndarray, _h: np.ndarray, SNR: float = 0.00000001, _BW: bool
         K = SNR
         # Getting the inverse filter
         H_w = (np.conj(H)) * (1/(abs(H)**2 + K))
+        # Getting the image back...
+        F_rec = G * H_w
+        # Coming back to spatial domain
+        f_rec = np.real(IFFT(F_rec))
+        return np.fft.fftshift(f_rec)
+    except:
+        trace()
+
+# Weiner Filtering...
+# param _img : noisy/blurry image
+# param f_ : estimate of original image (should be the same dim as _img)
+# param _h : estimate of deblurring function (PSF)
+# param _noise : estimate of noise distribution
+# param _BW : whether the image is black and white
+def weiner(_img: np.ndarray, _h: np.ndarray, _noise: np.ndarray, f_: np.ndarray, _BW: bool = True):
+    try:
+        _h /= _h.sum()
+        # Getting the optimal DFT size
+        __shp = _img.shape[:2]
+        '''
+        Getting fourier transforms (all needed)
+        Sensing
+        g = h*f + n (Real domain)
+        G = HF + N (Fourier Domain)
+        & weiner filter...
+        H* / (abs(H)**2 + K),
+        where K can be either of |N(u,v)|/|F(u,v)| or |N|/|F| (over complete matrix)
+        & recovered image  GH*
+        '''
+
+        # If the image is colored (BGR)
+        if not _BW:
+            __temp = np.zeros(_img.shape)
+            for _i in [0,1,2]:
+                __temp[:,:,_i] = weiner_(_img[:,:,_i], _h, _noise, f_[:,:,_i], True)
+            return __temp
+        # else
+        H = FFT(_h, __shp)
+        G = FFT(_img, __shp)
+        N = FFT(_noise, __shp)
+        F_ = FFT(f_, __shp)        
+        # Getting the inverse filter
+        H_w = (np.conj(H)) * (1/(abs(H)**2 + ((abs(N)**2)/(abs(F_)**2))))
         # Getting the image back...
         F_rec = G * H_w
         # Coming back to spatial domain
