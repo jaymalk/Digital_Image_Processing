3b4b7e4c907de04f39f6d4151caa5f76af9d4445
Rajbir Malik <cs1170416@iitd.ac.in>
2019-11-25 08:31:31 +0530

    Final working code, with data

--- 5/lib/npcolor.py
+++ 5/lib/npcolor.py
@@ -13,7 +13,7 @@ def nb(_shp: Tuple[int, int], _pt: Tuple[int, int], _arr: ArrayType, _tp: bool =
     # Getting the point
     _x, _y = _pt
     # Getting the shape
-    _m, _n = _shp
+    _m, _n = _shp[:2]
     # Creating the neighbourhood
     _nb = np.zeros(_shp)
     # Filling, acc. to type
@@ -41,7 +41,7 @@ def nb(_shp: Tuple[int, int], _pt: Tuple[int, int], _arr: ArrayType, _tp: bool =
 # Creating the neighbourhood of a template
 def create_nb(_template: ArrayType, _shp: Tuple[int, int] = (4, 4), _tp: bool = True):
     # Gettng the shape
-    _m, _n = _template.shape
+    _m, _n = _template.shape[:2]
     # Creating the lambda
     _l = lambda x: nb(_shp, (x//_n, x%_n), _template, _tp)
     # Creating the values
@@ -50,23 +50,15 @@ def create_nb(_template: ArrayType, _shp: Tuple[int, int] = (4, 4), _tp: bool = 
         _x.append(_l(i))
     _x = np.array(_x)
     # Reshaping the values
-    _x = _x.reshape((_m, _n, _shp[0], _shp[1]))
+
+    _x = _x.reshape([_m, _n]+list(_shp))
     # Returning the values
     return _x
 
 # Matching the best neighbourhoods
-def match_nb(_nb: ArrayType, _template_nbs: ArrayType):
+def match_nb_linear(_nb: ArrayType, _template_nbs: ArrayType, _rng:int = 0):
     # Creating matchings
-    _match = np.linalg.norm(np.linalg.norm(_template_nbs-_nb, axis=3), axis=2)
-    # Getting the argmin
-    _i = np.argmin(_match)
-    # Returning the best match index
-    return (_i//_template_nbs.shape[0], _i%_template_nbs.shape[1])
-
-# Matching the best neighbourhoods
-def match_nb_linear(_nb: ArrayType, _template_nbs: ArrayType, _rng: int = 0):
-    # Creating matchings
-    _match = np.linalg.norm(np.linalg.norm(_template_nbs-_nb, axis=2)[:, _rng:], axis=1)
+    _match = np.linalg.norm(np.linalg.norm(np.linalg.norm(_template_nbs-_nb, axis=3), axis=2)[:, _rng:], axis=1)
     # Getting the argmin
     _i = np.argmin(_match)
     # Returning the best match index
@@ -74,24 +66,24 @@ def match_nb_linear(_nb: ArrayType, _template_nbs: ArrayType, _rng: int = 0):
 
 # GLOBAL CONSTANTS
 global SIZEX, SIZEY, SHORT, LONG, FINAL, CLIP
-SIZEX = 128
-SIZEY = 128
-LONG = 40
-SHORT = 25
-FINAL = 200
-CLIP = 3000
+SIZEX = 64
+SIZEY = 64
+LONG = 30
+SHORT = 15
+FINAL = 128
+CLIP = 2000
 
 # Final Runner
 if __name__ == "__main__":
     # Read
     x = read_special(sys.argv[1], 'GIF')
     # Clipping
-    x = x[:SIZEY, :SIZEX].astype(np.float)/255
+    x = x[:SIZEY, :SIZEX, :3].astype(np.float)/255
 
     # Horizontal Elongation
 
     # Parameters
-    _shp = (SHORT, LONG)
+    _shp = (SHORT, LONG, 3)
     # Creating template-neighbourhood
     _xt = create_nb(x, _shp)
     # Linearising the data
@@ -99,23 +91,23 @@ if __name__ == "__main__":
     xx = x[_p%SIZEY, _p//SIZEX]
     _xxt = _xt[_p%SIZEY, _p//SIZEX]
     # Creating the new image
-    nx = np.zeros((SIZEY, FINAL))
+    nx = np.zeros((SIZEY, FINAL, 3))
     nx[:SIZEY, :SIZEX] = x
 
     # Creating the working lambda
     _i = SIZEX
-    __c = np.vectorize(lambda j: (xx[match_nb_linear(nb(_shp, (j, _i), nx), _xxt)] if j >= SHORT else xx[match_nb_linear(nb(_shp, (j, _i), nx), _xxt, SHORT-j)]))
+    __c = np.vectorize(lambda j: (match_nb_linear(nb(_shp, (j, _i), nx), _xxt) if j >= SHORT else match_nb_linear(nb(_shp, (j, _i), nx), _xxt, SHORT-j)))
     _r = np.arange(SIZEY-1, -1, -1, dtype=int)
     # Looping with lambda
     while _i < FINAL:
         print(f'C: {_i}', flush=True)
-        nx[_r, _i] = __c(_r)
+        nx[_r, _i] = xx[__c(_r)]
         _i += 1
 
 
     # Semi-Vertical Elongation
     # Creating the new image
-    hx = np.zeros((FINAL, FINAL))
+    hx = np.zeros((FINAL, FINAL, 3))
     hx[:SIZEY] = nx
 
     #Index
@@ -123,7 +115,7 @@ if __name__ == "__main__":
 
     # LEFT
     # Parameters
-    _shpl = (LONG, SHORT)
+    _shpl = (LONG, SHORT, 3)
     # Creating template-neighbourhood
     _xlt = create_nb(x, _shpl, False)
     # Linearising the data
@@ -131,12 +123,12 @@ if __name__ == "__main__":
     xxl = x[_p%SIZEY, _p//SIZEY]
     _xxlt = _xlt[_p%SIZEY, _p//SIZEY]
     # Creating working lambda
-    __cl = np.vectorize(lambda j: (xxl[match_nb_linear(nb(_shpl, (_i, j), hx, False), _xxlt)]))
+    __cl = np.vectorize(lambda j: (match_nb_linear(nb(_shpl, (_i, j), hx, False), _xxlt)))
     _l = np.arange(FINAL-SHORT, -1, -1, dtype=int)
 
     # RIGHT
     # Parameters
-    _shpr = (SHORT, LONG)
+    _shpr = (SHORT, LONG, 3)
     # Creating template-neighbourhood
     _xrt = create_nb(x, _shpr)
     # Linearising the data
@@ -144,19 +136,19 @@ if __name__ == "__main__":
     xxr = x[_p%SIZEY, _p//SIZEY]
     _xxrt = _xrt[_p%SIZEY, _p//SIZEY]
     # Creating the working lambda
-    __cr = np.vectorize(lambda j: (xxr[match_nb_linear(nb(_shpr, (_i, j), hx), _xxrt)]))
+    __cr = np.vectorize(lambda j: (match_nb_linear(nb(_shpr, (_i, j), hx), _xxrt)))
     _r = np.arange(FINAL-SHORT+1, FINAL, dtype=int)
     
     # Looping with lambdas
     while _i < FINAL:
         print(f'R: {_i}', flush=True)
-        hx[_i, _l] = __cl(_l)
-        hx[_i, _r] = __cr(_r)
+        hx[_i, _l] = xxl[__cl(_l)]
+        hx[_i, _r] = xxr[__cr(_r)]
         _i += 1
 
     # Writing the image
     try:
-        write(sys.argv[2], hx*512)
+        write(sys.argv[2], hx[:,:,::-1]*255)
     except IndexError:
-        write('final.png', hx*512)
-    write('real.png', x*512)
+        write('final.png', hx[:,:,::-1]*255)
+    write('real.png', x[:,:,::-1]*255)
--- 5/lib/texture.py
+++ 5/lib/texture.py
@@ -39,3 +39,36 @@ def texture_synthesis_steerable(_img: ArrayType, K: int, Q: int, _iter: int = 5,
         write(f'iter_{_}.png', _n*255)
     # Return the image
     return _n
+
+# Texture synthesis using steerable pyramids
+def texture_synthesis_steerable_C(_imgF: ArrayType, K: int, Q: int, _iter: int = 5, _shp: Tuple[int, int] = None) -> ArrayType:
+    _M = np.random.normal(0.5, 0.4, _imgF.shape[:2]) if _shp == None else np.random.normal(0.5, 0.4, _shp)
+    _F = np.zeros(_imgF.shape) if _shp == None else np.zeros(list(_shp)+[3])
+    for ii in [0,1,2]:
+        _img = _imgF[:,:,ii]
+        # Creating image steerable pyramids
+        _p = pyramids_fast(_img, K, Q, _cvt=False)
+        # Creating the noise
+        _n = _M
+        # Matching the histogram
+        _n = match_hist(_n, _img)
+        # Loop
+        for _ in range(_iter):
+            # Creating the steerable pyramids
+            _pn = pyramids_fast(_n, K, Q, _cvt=False)
+            # Matching histograms
+                # H0
+            _pn[0][0] = match_hist(_pn[0][0], _p[0][0])
+                # Final LOW
+            _pn[-1][0] = match_hist(_pn[-1][0], _p[-1][0])
+                # Orientations
+            for i in range(K):
+                for j in range(Q):
+                    _pn[i+1][j] = match_hist(_pn[i+1][j], _p[i+1][j])
+            # Recreating the image
+            _n = recreate_fast(_pn)
+            # Matching the histograms
+            _n = match_hist(_n, _img)
+        _F[:,:,ii] = _n
+    # Return the image
+    return _F
